0699a03c8b5123567f281e56520235ad81183a20
Jack Walker <Jack.Walks@gmail.com>
2017-05-09 00:33:22 +1000

    Initial Commit

---
+++ Study Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study Buddy
+//
+//  Created by Jack Walker on 9/5/17.
+//  Copyright © 2017 Jack Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
